ea1f085fb452a423784979754a057f293af5aef4
JeanCarlosSC <jeancarlosodi@gmail.com>
2023-10-01 17:35:29 -0500

    adding shuffle method

--- sRAD.js
+++ sRAD.js
@@ -3,3 +3,23 @@
 function toCOP(value) {
     return value.toLocaleString('es-CO', {currency: 'COP', style: 'currency'})
 }
+
+// sort an array randomly
+function shuffle(array) {
+    let currentIndex = array.length,  randomIndex;
+  
+    // While there remain elements to shuffle.
+    while (currentIndex > 0) {
+  
+      // Pick a remaining element.
+      randomIndex = Math.floor(Math.random() * currentIndex);
+      currentIndex--;
+  
+      // And swap it with the current element.
+      [array[currentIndex], array[randomIndex]] = [
+        array[randomIndex], array[currentIndex]];
+    }
+  
+    return array;
+  }
+  
